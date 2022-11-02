a2556527c3c25b5fcb1ada109b1463784dda03ff
jurtasunel <114501562+jurtasunel@users.noreply.github.com>
2022-11-02 10:20:06 +0000

    Update constants.R

--- constants.R
+++ constants.R
@@ -1,3 +1,4 @@
+library(treeio) # Many functions to read in different tree formats, includes raxml with bootstrap.
 library(seqinr) # Deal with fasta files.
 library(ggplot2)
 library(dplyr) # Format dataframes.
@@ -5,14 +6,35 @@ library(lubridate) # Deal with date format.
 library(sf) # Deal with maps in R.
 library(ape) # Read tree files.
 library(tidyverse)
-library(dplyr)
+library(gridExtra)
+library(ggtree)
+library(ggrepel)
+library(lemon) # Format nice looking data frames and tables.
+library(readODS)
 
-fasta_path = "/home/gabriel/Desktop/Jose/Projects/GISAID_reports/Data/August/gisaid_hcov-19_2022_09_29_15.fasta"
+# Load fasta file and metadata files.
+fasta_path = "XXXX"
 fasta_file <- read.fasta(fasta_path, as.string = TRUE, forceDNAtolower = FALSE, set.attributes = FALSE)
-metadata_path = "/home/gabriel/Desktop/Jose/Projects/GISAID_reports/Data/August/gisaid_hcov-19_2022_09_29_15.tsv"
+metadata_path = "YYYY"
 metadata_file <- read.table(metadata_path,  sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-map_path = "/home/gabriel/Desktop/Jose/Maps/26 Counties/Census2011_Admin_Counties_generalised20m.shp"
-# If .shp map doesn't exist or is missing some files, open with openjump and safe as shape file to generate all required files.
+lab_path = "ZZZZ"
+lab_file <- read.table(lab_path,  sep = '\t', header = TRUE, stringsAsFactors = FALSE, quote = "£")
+# Load laboratory tags file.
+labtags_file <- read_ods("path/to/labtagsfile.ods", strings_as_factors = FALSE)
+# Load map of Ireland file. The .shp file needs to be on same directory as .shx, .dbf, .cpg and .prj files.
+# If .shp map doesn't exist or is missing some files, open with openjump and save as shape file to generate all required files.
+map_path = "path/to/mapfile.shp"
 ROI_map <- st_read(map_path)
+# Make a list of mutations to report.
+target_mutations <- list()
+target_mutations[[1]] = c("mutA", "mutB")
+target_mutations[[2]] = c("mutC")
+target_mutations[[3]] = c("mutA", "mutB", "mutC")
+# Get mafft and raxml commands on variables.
+# mafft documentation: https://towardsdatascience.com/how-to-perform-sequence-alignment-on-2019-ncov-with-mafft-96c1944da8c6
+mafft_command = paste0("mafft --auto --reorder ", fasta_path, " > mafft_aligned.fasta")
+# raxml cocumentation: https://isu-molphyl.github.io/EEOB563/computer_labs/lab4/raxml-ng.html
+# raxml github installation : https://github.com/amkozlov/raxml-ng
+raxml_command = "/usr/bin/raxmlHPC-PTHREADS-AVX /usr/share/man/man1/raxmlHPC-PCTHREADS-AVX -T 12 -f a -x 123 -p 123 -N 100 -m GTRCAT -k -O -s mafft_aligned.fasta -n raxml_tree -w `pwd`"
 
 
